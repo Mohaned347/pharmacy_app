5ddcda478098c70409412566aa6fd385246b3ab6
Mohaned Tareq Husain <110483102+Mohaned347@users.noreply.github.com>
2024-12-24 19:04:50 +0200

    add changes

--- Form2.cs
+++ Form2.cs
@@ -10,17 +10,17 @@ using System.Windows.Forms;
 
 namespace main_APP
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2();
-            form2.ShowDialog();
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
             this.Close();
         }
     }
