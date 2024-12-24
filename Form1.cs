1b0d72fa928d226e0120797bb3b320681a154893
Mohaned Tareq Husain <110483102+Mohaned347@users.noreply.github.com>
2024-12-24 21:11:10 +0200

    comet

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pharmacy
+namespace main_APP
 {
     public partial class Form1 : Form
     {
--- Form2.cs
+++ Form2.cs
@@ -19,6 +19,8 @@ namespace main_APP
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
             Form2 form2 = new Form2();
             form2.Hide();
         }
