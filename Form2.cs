926b23ccf6a00b18836e15efbf64be54d9694c55
Mohaned Tareq Husain <110483102+Mohaned347@users.noreply.github.com>
2024-12-24 21:10:22 +0200

    add

--- Form2.cs
+++ Form2.cs
@@ -19,8 +19,6 @@ namespace main_APP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form1 form1 = new Form1();
-            form1.ShowDialog();
             Form2 form2 = new Form2();
             form2.Hide();
         }
