0e19eef9771f2dd3e94c4379ef91f169566b10b9
mahmoud <ytop2395@gmail.com>
2024-12-25 00:23:11 +0200

    test

--- Form4.cs
+++ Form4.cs
@@ -25,20 +25,76 @@ namespace main_APP
 
         private void Done_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=LAPTOP-DPIBDA8N;Initial Catalog=pharmacy;Integrated Security=True");
-            SqlCommand cmd = new SqlCommand("select * from employee where [User Name] = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            using (SqlConnection con = new SqlConnection("YourConnectionStringHere"))
+            {
+                string name = MedicineBox.Text;
+                int quantity = int.Parse(QuantityBox.Text);
 
-            con.Open();
-            SqlDataReader dr = cmd.ExecuteReader();
+                using (SqlCommand cmd = new SqlCommand("SELECT Quantity, MedicineID FROM Medicine WHERE Name = @Name", con))
+                {
+                    cmd.Parameters.AddWithValue("@Name", name);
+
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        int availableQuantity = (int)reader["Quantity"];
+                        int medicineID = (int)reader["MedicineID"];
+
+                        if (quantity <= availableQuantity)
+                        {
+                            // Update Medicine table
+                            reader.Close();
+                            using (SqlCommand updateCmd = new SqlCommand("UPDATE Medicine SET Quantity = Quantity - @Quantity WHERE Name = @Name", con))
+                            {
+                                updateCmd.Parameters.AddWithValue("@Quantity", quantity);
+                                updateCmd.Parameters.AddWithValue("@Name", name);
+
+                                updateCmd.ExecuteNonQuery();
+                            }
+
+                            // Insert Sale record
+                            using (SqlCommand insertCmd = new SqlCommand("INSERT INTO Sale (Quantity, SaleDate, MedicineID) VALUES (@Quantity, @SaleDate, @MedicineID)", con))
+                            {
+                                insertCmd.Parameters.AddWithValue("@Quantity", quantity);
+                                insertCmd.Parameters.AddWithValue("@SaleDate", DateTime.Now);
+                                insertCmd.Parameters.AddWithValue("@MedicineID", medicineID);
+
+                                insertCmd.ExecuteNonQuery();
+                            }
+
+                            MessageBox.Show("Quantity updated and sale recorded successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Insufficient quantity.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Medicine does not exist.");
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=LAPTOP-DPIBDA8N;Initial Catalog=pharmacy;Integrated Security=True");
-            SqlCommand cmd = new SqlCommand("select * from employee where [User Name] = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+            using (SqlConnection con = new SqlConnection("Data Source=LAPTOP-DPIBDA8N;Initial Catalog=pharmacy;Integrated Security=True"))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Customer (Name, ContactNumber, Address) VALUES (@Name, @ContactNumber, @Address)", con))
+                {
+                    // Add parameters to avoid SQL injection
+                    cmd.Parameters.AddWithValue("@Name", CusmterNameBox.Text);
+                    cmd.Parameters.AddWithValue("@ContactNumber", ContactBox.Text);
+                    cmd.Parameters.AddWithValue("@Address", PersIDbox.Text);
 
-            con.Open();
-            SqlDataReader dr = cmd.ExecuteReader();
+                    // Execute the command
+                    con.Open();  // Open the connection
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
