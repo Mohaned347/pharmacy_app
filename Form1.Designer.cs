518e05481c2538faa8e1ec79a13cfda157ba6bbd
Mohaned Tareq Husain <110483102+Mohaned347@users.noreply.github.com>
2024-12-24 21:57:28 +0200

    add forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,45 +30,78 @@ namespace main_APP
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.UserBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(266, 196);
+            this.button1.Location = new System.Drawing.Point(261, 289);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(262, 67);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // UserBox
             // 
-            this.button2.Location = new System.Drawing.Point(266, 312);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(262, 74);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.UserBox.Location = new System.Drawing.Point(302, 125);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(193, 20);
+            this.UserBox.TabIndex = 1;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(302, 209);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(193, 20);
+            this.PassBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "userName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "password";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.UserBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox UserBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
