e514319d72ce279cd5d5705ac721fe4017b66e33
mahmoud <ytop2395@gmail.com>
2024-12-24 23:14:29 +0200

    test

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -41,118 +41,145 @@
             this.Medicine = new System.Windows.Forms.Label();
             this.Total_Cost = new System.Windows.Forms.Label();
             this.Done = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CusmterNameBox
             // 
-            this.CusmterNameBox.Location = new System.Drawing.Point(151, 93);
+            this.CusmterNameBox.Location = new System.Drawing.Point(121, 74);
+            this.CusmterNameBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CusmterNameBox.Name = "CusmterNameBox";
-            this.CusmterNameBox.Size = new System.Drawing.Size(177, 22);
+            this.CusmterNameBox.Size = new System.Drawing.Size(142, 20);
             this.CusmterNameBox.TabIndex = 0;
             // 
             // MedicineBox
             // 
-            this.MedicineBox.Location = new System.Drawing.Point(518, 177);
+            this.MedicineBox.Location = new System.Drawing.Point(414, 142);
+            this.MedicineBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MedicineBox.Name = "MedicineBox";
-            this.MedicineBox.Size = new System.Drawing.Size(177, 22);
+            this.MedicineBox.Size = new System.Drawing.Size(142, 20);
             this.MedicineBox.TabIndex = 1;
             // 
             // PersIDbox
             // 
-            this.PersIDbox.Location = new System.Drawing.Point(151, 177);
+            this.PersIDbox.Location = new System.Drawing.Point(121, 142);
+            this.PersIDbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PersIDbox.Name = "PersIDbox";
-            this.PersIDbox.Size = new System.Drawing.Size(177, 22);
+            this.PersIDbox.Size = new System.Drawing.Size(142, 20);
             this.PersIDbox.TabIndex = 2;
             // 
             // ContactBox
             // 
-            this.ContactBox.Location = new System.Drawing.Point(518, 93);
+            this.ContactBox.Location = new System.Drawing.Point(414, 74);
+            this.ContactBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ContactBox.Name = "ContactBox";
-            this.ContactBox.Size = new System.Drawing.Size(177, 22);
+            this.ContactBox.Size = new System.Drawing.Size(142, 20);
             this.ContactBox.TabIndex = 3;
             // 
             // QuantityBox
             // 
-            this.QuantityBox.Location = new System.Drawing.Point(151, 251);
+            this.QuantityBox.Location = new System.Drawing.Point(121, 201);
+            this.QuantityBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.QuantityBox.Name = "QuantityBox";
-            this.QuantityBox.Size = new System.Drawing.Size(177, 22);
+            this.QuantityBox.Size = new System.Drawing.Size(142, 20);
             this.QuantityBox.TabIndex = 4;
             // 
             // TotalCostBox
             // 
-            this.TotalCostBox.Location = new System.Drawing.Point(518, 251);
+            this.TotalCostBox.Location = new System.Drawing.Point(414, 201);
+            this.TotalCostBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TotalCostBox.Name = "TotalCostBox";
-            this.TotalCostBox.Size = new System.Drawing.Size(177, 22);
+            this.TotalCostBox.Size = new System.Drawing.Size(142, 20);
             this.TotalCostBox.TabIndex = 5;
             // 
             // Cusmter_Name
             // 
             this.Cusmter_Name.AutoSize = true;
-            this.Cusmter_Name.Location = new System.Drawing.Point(29, 99);
+            this.Cusmter_Name.Location = new System.Drawing.Point(23, 79);
+            this.Cusmter_Name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Cusmter_Name.Name = "Cusmter_Name";
-            this.Cusmter_Name.Size = new System.Drawing.Size(96, 16);
+            this.Cusmter_Name.Size = new System.Drawing.Size(77, 13);
             this.Cusmter_Name.TabIndex = 6;
-            this.Cusmter_Name.Text = "Cusmter Name";
+            this.Cusmter_Name.Text = "Custmer Name";
             // 
             // Contact
             // 
             this.Contact.AutoSize = true;
-            this.Contact.Location = new System.Drawing.Point(419, 99);
+            this.Contact.Location = new System.Drawing.Point(335, 79);
+            this.Contact.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Contact.Name = "Contact";
-            this.Contact.Size = new System.Drawing.Size(52, 16);
+            this.Contact.Size = new System.Drawing.Size(45, 13);
             this.Contact.TabIndex = 7;
             this.Contact.Text = "Contact";
             // 
             // Address
             // 
             this.Address.AutoSize = true;
-            this.Address.Location = new System.Drawing.Point(29, 183);
+            this.Address.Location = new System.Drawing.Point(23, 146);
+            this.Address.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Address.Name = "Address";
-            this.Address.Size = new System.Drawing.Size(58, 16);
+            this.Address.Size = new System.Drawing.Size(46, 13);
             this.Address.TabIndex = 8;
             this.Address.Text = "Address";
             // 
             // Quantity
             // 
             this.Quantity.AutoSize = true;
-            this.Quantity.Location = new System.Drawing.Point(29, 257);
+            this.Quantity.Location = new System.Drawing.Point(23, 206);
+            this.Quantity.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Quantity.Name = "Quantity";
-            this.Quantity.Size = new System.Drawing.Size(55, 16);
+            this.Quantity.Size = new System.Drawing.Size(49, 13);
             this.Quantity.TabIndex = 9;
             this.Quantity.Text = "Quantity";
             // 
             // Medicine
             // 
             this.Medicine.AutoSize = true;
-            this.Medicine.Location = new System.Drawing.Point(419, 183);
+            this.Medicine.Location = new System.Drawing.Point(335, 146);
+            this.Medicine.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Medicine.Name = "Medicine";
-            this.Medicine.Size = new System.Drawing.Size(62, 16);
+            this.Medicine.Size = new System.Drawing.Size(48, 13);
             this.Medicine.TabIndex = 10;
             this.Medicine.Text = "Medicine";
             // 
             // Total_Cost
             // 
             this.Total_Cost.AutoSize = true;
-            this.Total_Cost.Location = new System.Drawing.Point(419, 257);
+            this.Total_Cost.Location = new System.Drawing.Point(335, 206);
+            this.Total_Cost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Total_Cost.Name = "Total_Cost";
-            this.Total_Cost.Size = new System.Drawing.Size(68, 16);
+            this.Total_Cost.Size = new System.Drawing.Size(56, 13);
             this.Total_Cost.TabIndex = 11;
             this.Total_Cost.Text = "Total Cost";
             // 
             // Done
             // 
-            this.Done.Location = new System.Drawing.Point(346, 354);
+            this.Done.Location = new System.Drawing.Point(414, 274);
+            this.Done.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Done.Name = "Done";
-            this.Done.Size = new System.Drawing.Size(96, 37);
+            this.Done.Size = new System.Drawing.Size(77, 30);
             this.Done.TabIndex = 12;
             this.Done.Text = "Done";
             this.Done.UseVisualStyleBackColor = true;
+            this.Done.Click += new System.EventHandler(this.Done_Click);
             // 
-            // Form1
+            // button1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.button1.Location = new System.Drawing.Point(121, 274);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 30);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Done);
             this.Controls.Add(this.Total_Cost);
             this.Controls.Add(this.Medicine);
@@ -166,7 +193,8 @@
             this.Controls.Add(this.PersIDbox);
             this.Controls.Add(this.MedicineBox);
             this.Controls.Add(this.CusmterNameBox);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form4";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -188,5 +216,6 @@
         private System.Windows.Forms.Label Medicine;
         private System.Windows.Forms.Label Total_Cost;
         private System.Windows.Forms.Button Done;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace main_APP
 {
@@ -20,6 +21,24 @@ namespace main_APP
         private void Form4_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Done_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-DPIBDA8N;Initial Catalog=pharmacy;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from employee where [User Name] = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-DPIBDA8N;Initial Catalog=pharmacy;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from employee where [User Name] = '" + textBox1.Text + "' and Password = '" + textBox2.Text + "'", con);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
         }
     }
 }
